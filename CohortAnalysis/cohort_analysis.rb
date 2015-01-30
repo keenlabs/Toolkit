66deb340d73a4d791b4f0c6f948a0d177b12af8c
Maria Dumanis <maria@keen.io>
2015-01-29 16:45:00 -0800

    added loop for funnel

--- CohortAnalysis/cohort_analysis.rb
+++ CohortAnalysis/cohort_analysis.rb
@@ -1,18 +1,27 @@
 require 'keen'
-require 'active_support/all' #for datetime calculation e.g. weeks.ago.at_beginning_of_week
+require 'active_support/all'
 require 'date'
 
 Keen.project_id = '5011efa95f546f2ce2000000'
-Keen.read_key = 'ef717eaec4aeb8e8b8b18891ffaa1eafed8c14cac1f7cb90030eaa7ed79ed2d540ee267547267c73f4c6e7ff9bcb8f7dec4df9500499a70737777d71971a20e6e3cd6532b44a44c5d269811178c2308867fd5082d44e51c05496b0099c4a06649207b8db43fe0458cf3cc059f5ecaadc'
+Keen.read_key = '' #Key has been removed for security purposes
 
-puts signup_start_date = Date.new(2015, 1, 5) #{}"2015-01-05 00:00:00 -0800"  # monday morning at midnight
-puts one_month_out_start =  signup_start_date + 7
+signup_start_date = Date.new(2015, 1, 5) # "2015-01-05 00:00:00 -0800"  # monday morning at midnight
+#puts Date.today.at_beginning_of_week
+#puts signup_start_date < Date.today.at_beginning_of_week
 
+while signup_start_date < Date.today.at_beginning_of_week do 
 
-puts Keen.funnel(:steps => [{ 
+  puts "start date = ", signup_start_date
+
+  puts Keen.funnel(:steps => [{ 
   :actor_property => "organization.id", 
   :event_collection => "create_organization", #Signups
-  :timeframe => "previous_day" },
+  :timeframe => {   # This defines the timeframe for the first step in the funnel 
+    :start => signup_start_date,  
+    :end => signup_start_date + 7,
+    },
+  },
+
   {
   :actor_property => "project.organization.id", 
   :event_collection => "events_added_api_call", #Data Sent
@@ -20,21 +29,69 @@ puts Keen.funnel(:steps => [{
     :start => signup_start_date,  
     :end => signup_start_date + 7,
   	},
-  :optional => true }, 
+  :optional => true
+  }, 
+
+  {
+  :actor_property => "project.organization.id", 
+  :event_collection => "events_added_api_call", #Data Sent
+  :timeframe => {  
+    :start => signup_start_date + 7,  
+    :end => signup_start_date + 14,
+    },
+  :optional => true
+  }, 
+
   {
   :actor_property => "project.organization.id", 
   :event_collection => "analysis_api_call", #Query
-  :timeframe => "previous_day",
-  :optional => true  }, 
+  :timeframe => {   
+    :start => signup_start_date,  
+    :end => signup_start_date + 7,
+    },
+  :optional => true  
+  }, 
+
+  {
+  :actor_property => "project.organization.id", 
+  :event_collection => "analysis_api_call", #Query
+  :timeframe => {    
+    :start => signup_start_date + 7,  
+    :end => signup_start_date + 14,
+    },
+  :optional => true  
+  }, 
+
   {
   :actor_property => "organization.id", 
   :event_collection => "submit_payment", #Paying Customer
-  :timeframe => "previous_day", 
+  :timeframe => {   
+    :start => signup_start_date,  
+    :end => signup_start_date + 7,
+    },
   :filters => [{
     "property_name" => "plan",
     "operator" => "not_contains",
     "property_value" => "FREE"
   	}],
-  }
+  :optional => true  
+  },
 
+  {
+  :actor_property => "organization.id", 
+  :event_collection => "submit_payment", #Paying Customer
+  :timeframe => {   
+    :start => signup_start_date + 7,  
+    :end => signup_start_date + 14,
+    },
+  :filters => [{
+    "property_name" => "plan",
+    "operator" => "not_contains",
+    "property_value" => "FREE"
+    }],
+  }
   ]) 
+
+  signup_start_date = signup_start_date + 7
+end
+
